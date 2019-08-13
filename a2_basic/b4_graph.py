cc6f107578ea7dda1c6c515134bc4d69b9861b0b
freshield <zxdsw199182@gmail.com>
2019-08-13 22:00:30 +0800

    done the les miserables

--- a2_basic/b4_graph.py
+++ a2_basic/b4_graph.py
@@ -61,4 +61,52 @@ def graph_with_opts():
 # c = graph_with_opts()
 # c.render('data/b4_graph_node_link.html')
 
-def graph_weibo()
+def graph_weibo():
+    with open('data/weibo.json', 'r', encoding='utf-8') as f:
+        j = json.load(f)
+        nodes, links, categories, cont, mid, userl = j
+
+    c = (
+        Graph()
+        .add('', nodes, links, categories, repulsion=50,
+             linestyle_opts=opts.LineStyleOpts(curve=0.2),
+             label_opts=opts.LabelOpts(is_show=False))
+        .set_global_opts(
+            legend_opts=opts.LegendOpts(is_show=False),
+            title_opts=opts.TitleOpts(title='Graph-weibo')
+        )
+    )
+
+    return c
+
+
+# c = graph_weibo()
+# c.render('data/b4_graph_weibo.html')
+
+
+def graph_les_miserables():
+    with open('data/les-miserables.json', 'r', encoding='utf-8') as f:
+        j = json.load(f)
+        nodes = j['nodes']
+        links = j['links']
+        categories = j['categories']
+
+    c = (
+        Graph(init_opts=opts.InitOpts(width='1000px', height='600px'))
+        .add('', nodes=nodes, links=links, categories=categories,
+             layout='circular', is_rotate_label=True,
+             linestyle_opts=opts.LineStyleOpts(color='source', curve=0.3),
+             label_opts=opts.LabelOpts(position='right'))
+        .set_global_opts(
+            title_opts=opts.TitleOpts(title='Graph-Les Miserables'),
+            legend_opts=opts.LegendOpts(
+                orient='vertical', pos_left='2%', pos_top='20%'
+            )
+        )
+    )
+
+    return c
+
+
+c = graph_les_miserables()
+c.render('data/b4_graph_les_miserables.html')
