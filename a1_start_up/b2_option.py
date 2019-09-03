5e83de380ceb8795d40e804953d23407beae2d79
freshield <zxdsw199182@gmail.com>
2019-09-03 20:46:35 +0800

    done b2 options

--- a1_start_up/b2_option.py
+++ a1_start_up/b2_option.py
@@ -1,5 +1,12 @@
+from pyecharts.charts import Bar
+from pyecharts import options as opts
+
 
 bar = (
-    Bar().add_xaxis(['衬衫','羊毛衫','雪纺衫','裤子','高跟鞋','袜子']
-                    ).add_yaxis('商家A', [5,20,36,10,75,90])
+    Bar()
+    .add_xaxis(['衬衫','羊毛衫','雪纺衫','裤子','高跟鞋','袜子'])
+    .add_yaxis('商家A', [5,20,36,10,75,90])
+    .set_global_opts(title_opts={'text':'主标题', 'subtext':'副标题'})
 )
+
+bar.render('data/b2_options.html')
