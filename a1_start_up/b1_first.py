312a5d6597e16a9246095272e2304f8b08138945
freshield <zxdsw199182@gmail.com>
2019-09-03 20:43:18 +0800

    begin startup

---
+++ a1_start_up/b1_first.py
@@ -0,0 +1,16 @@
+from pyecharts.charts import Bar
+
+bar = Bar()
+bar.add_xaxis(['衬衫','羊毛衫','雪纺衫','裤子','高跟鞋','袜子'])
+bar.add_yaxis('商家A', [5,20,36,10,75,90])
+
+bar = (
+    Bar().add_xaxis(['衬衫','羊毛衫','雪纺衫','裤子','高跟鞋','袜子']
+                    ).add_yaxis('商家A', [5,20,36,10,75,90])
+)
+
+bar = Bar().add_xaxis(['衬衫','羊毛衫','雪纺衫','裤子','高跟鞋','袜子']
+                    ).add_yaxis('商家A', [5,20,36,10,75,90])
+
+
+bar.render('data/b1_first.html')
